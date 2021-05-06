f4551bb4adc8d6c381bb62fbc46e9d42c1c71895
WiseAdvice11 <breweroff@gmail.com>
2021-05-06 18:50:18 +0300

    add Create

--- src/main/java/ru/a1/bookDemo/controller/HomeController.java
+++ src/main/java/ru/a1/bookDemo/controller/HomeController.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.a1.bookDemo.model.Book;
 import ru.a1.bookDemo.repo.BookRepository;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
     private BookRepository bookRepository;
@@ -17,9 +19,16 @@ public class HomeController {
         this.bookRepository = bookRepository;
     }
 
-    @GetMapping("/")
+    @GetMapping
     public String getIndex(Model model)  {
         model.addAttribute("books", bookRepository.findAll());
+        model.addAttribute("newbook",new Book());
         return "index";
     }
+    @PostMapping
+    public String createBook (Book book){
+bookRepository.save(book);
+        return "redirect:/";
+    }
+
 }
